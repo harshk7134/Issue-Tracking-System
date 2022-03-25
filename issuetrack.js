43039b7c85f40cf317113c3b8fad0a914895e21a
harshk7134 <101197903+harshk7134@users.noreply.github.com>
2022-03-25 16:50:33 +0530

    Add files via upload

---
+++ issuetrack.js
@@ -0,0 +1,86 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('outline').value;
+  var issueSeverity = document.getElementById('wid').value;
+  var issueAssignedTo = document.getElementById('ass').value;
+  var issueId = document.getElementById('mail').value;
+  var issueStatus = 'Open';
+  
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+  
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));    
+  }
+  else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+  
+  document.getElementById('issueInputForm').reset();
+  
+  fetchIssues();
+  
+  e.preventDefault();
+}
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+  
+  localStorage.setItem('issues', JSON.stringify(issues));
+  
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+  
+  localStorage.setItem('issues', JSON.stringify(issues));
+  
+  fetchIssues();
+}
+
+function fetchIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  
+  var issuesList = document.getElementById('issuesList');
+  
+  issuesList.innerHTML = '';
+  
+  for (var i = issues.length-1; i>=0; i--) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var severity = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+    
+    issuesList.innerHTML += '<div class = "well">' +
+                        '<div id="idMail"><h6>EMail ID: ' + id + '</h6></div>' + 
+                        '<h5><span class = "label-info"><b>Status:</b>' + status + '</span></h5>' +
+                        '<h3 id="descrip">' + desc + '</h3>' +
+                        '<h6><span class = "glypicon-time"></span><b>Priority:</b> ' + severity + '</h6>' +
+                        '<h6><span class = "glypicon-user"></span><b>Assigned To:</b> ' + assignedTo + '</h6>' + 
+                        '<div class="opcl"><a onclick = "setStatusClosed(\''+id+'\')" class = "btn btn-warning">Close</a> ' +
+                        '<a onclick = "deleteIssue(\''+id+'\')" class = "btn btn-danger">Delete</a></div>' +
+                         '</div>';
+  }
+}
